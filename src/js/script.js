c5131fb7b73e367dd868cbe011a8f0a15dff591d
Olga <olga.coin@ya.ru>
2025-02-26 02:05:09 +0300

    start3-1

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,21 @@
 $(document).ready(function () {
 
+	// sticky menu + toTop
+
+	$(window).scroll(function () {
+
+		if ($(this).scrollTop() >= 220) {
+			$('.header__bottom').addClass('fixed');
+		} else {
+			$('.header__bottom').removeClass('fixed');
+		}
+
+		// if ($(this).scrollTop() >= 3000) {
+		// 	$('.toTop').addClass('show');
+		// } else {
+		// 	$('.toTop').removeClass('show');
+		// }
+	});
 
 
 
