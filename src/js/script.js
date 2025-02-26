7c4bac07c552971120df862f69993b39aadc0cae
Olga <olga.coin@ya.ru>
2025-02-26 19:33:51 +0300

    header+popup

--- src/js/script.js
+++ src/js/script.js
@@ -18,6 +18,15 @@ $(document).ready(function () {
 	});
 
 
+	// popup
+
+	$(".click-here").on('click', function () {
+		$(".custom-model-main").addClass('model-open');
+	});
+	$(".close-btn, .bg-overlay").click(function () {
+		$(".custom-model-main").removeClass('model-open');
+	});
+
 
 });
 
