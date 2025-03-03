26d4c9c7f513708c9294f154b225a92cfc7dee87
Olga <olga.coin@ya.ru>
2025-03-03 18:08:49 +0300

    sale-slider

--- src/js/script.js
+++ src/js/script.js
@@ -106,6 +106,45 @@ $(document).ready(function () {
 		]
 	});
 
+	$('.sale__slider').slick({
+		speed: 800,
+		slidesToShow: 4,
+		slidesToScroll: 1,
+		// autoplay: true,
+		prevArrow: $('.s-prev'),
+		nextArrow: $('.s-next'),
+		responsive: [
+			{
+				breakpoint: 992,
+				settings: {
+					slidesToShow: 3,
+					slidesToScroll: 1
+				}
+			},
+			{
+				breakpoint: 768,
+				settings: {
+					slidesToShow: 2,
+					slidesToScroll: 1
+				}
+			},
+			{
+				breakpoint: 576,
+				settings: {
+					slidesToShow: 2,
+					slidesToScroll: 1
+				}
+			},
+			{
+				breakpoint: 480,
+				settings: {
+					slidesToShow: 1,
+					slidesToScroll: 1
+				}
+			}
+		]
+	});
+
 
 	// product counter
 	function inputNumber(e) {
@@ -152,7 +191,14 @@ $(document).ready(function () {
 	inputNumber($('.counter__input'));
 
 
-
+	var mh = 0;
+	$(".section-gallery__item").each(function () {
+		var h_block = parseInt($(this).height());
+		if (h_block > mh) {
+			mh = h_block;
+		};
+	});
+	$(".section-gallery__item").height(mh);
 
 
 });
