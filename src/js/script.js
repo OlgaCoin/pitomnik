2cdfd07080abaa60f472b3cfd5bbc1a8d2cc4bf9
Olga <olga.coin@ya.ru>
2025-03-09 05:48:24 +0300

    cta2

--- src/js/script.js
+++ src/js/script.js
@@ -440,6 +440,46 @@ $(document).ready(function () {
 	// 	}
 	// });
 
+	// catalogs slider
+	$('.catalogs__slider').slick({
+		speed: 800,
+		slidesToShow: 4,
+		slidesToScroll: 1,
+		// autoplay: true,
+		prevArrow: $('.c-prev'),
+		nextArrow: $('.c-next'),
+		responsive: [
+			{
+				breakpoint: 992,
+				settings: {
+					slidesToShow: 3,
+					slidesToScroll: 1
+				}
+			},
+			{
+				breakpoint: 768,
+				settings: {
+					slidesToShow: 2,
+					slidesToScroll: 1
+				}
+			},
+			{
+				breakpoint: 576,
+				settings: {
+					slidesToShow: 2,
+					slidesToScroll: 1
+				}
+			},
+			{
+				breakpoint: 480,
+				settings: {
+					slidesToShow: 1,
+					slidesToScroll: 1
+				}
+			}
+		]
+	});
+
 
 	// product counter
 	function inputNumber(e) {
